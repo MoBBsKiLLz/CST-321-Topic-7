fbff5bae5add77ea45058a92e32198394b513a38
Miguel Zepeda <migg_18@live.com>
2023-10-12 17:48:18 -0700

    updated user management

--- usermng.sh
+++ usermng.sh
@@ -6,13 +6,37 @@ group=$2
 if test $# -gt 0; then
     case "$3" in
         -a) 
-            echo "add  user"
-            while read username password; do
-                echo "Username: $username Password: $password"
-            done <$file
+            if [ $(getent group $group) ]; then
+                while read username password; do
+                    egrep "^$username" /etc/passwd >/dev/null
+                    if [ $? -eq 0 ]; then
+                        echo "$username exists!"
+                    else
+                        useradd -m -p "$password" "$username"
+                        usermod -a -G "$group" "$username"
+                        [ $? -eq 0 ] && echo "User has been added to system!" || echo "Failed to add a user!"
+                    fi
+                done <$file
+            else
+                while read username password; do
+                    egrep "^$username" /etc/passwd >/dev/null
+                    if [ $? -eq 0 ]; then
+                        echo "$username exists!"
+                    else
+                        groupadd "$group"
+                        useradd -m -p "$password" "$username"
+                        usermod -a -G "$group" "$username"
+                        [ $? -eq 0 ] && echo "User has been added to system!" || echo "Failed to add a user!"
+                    fi
+                done <$file
+            fi
             ;;
         -r) 
-            echo "remove user"
+            while read username password; do
+                userdel "$username"
+                userdel -r "$username"
+                [ $? -eq 0 ] && echo "User has been removed from the system!" || echo "Failed to remove a user!"
+            done <$file
             ;;
     esac
 fi
